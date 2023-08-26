d3242e00fb7b7a65d6ffb42e3cdc522972555905
Oscar Ossa <oskrlex17@gmail.com>
2023-08-26 13:38:57 -0500

    fix(app): fix some things

--- main.go
+++ main.go
@@ -14,18 +14,23 @@ func main() {
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(checkTokenMiddleware)
 	// e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 	// 	AllowOrigins: []string{"https://red-planet.com.co/", "138.128.160.43"},
 	// 	AllowMethods: []string{http.MethodPost},
 	// }))
 
-	e.POST("/api/promise", handlerPromise)
+	e.POST("/api/promise", createPromise)
+
+	e.Start(":9000")
 }
 
-func handlerPromise(c echo.Context) error {
+func createPromise(c echo.Context) error {
 	if err := c.Bind(&paymentPromise); err != nil {
 		return c.String(http.StatusBadRequest, "Bad Request")
 	}
+
+	createPaymentPromise(paymentPromise)
 
 	return c.String(http.StatusOK, "Promise Recieved")
 }
